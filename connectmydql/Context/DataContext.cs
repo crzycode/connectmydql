8e8ad697559a1804126890817fe5807f5da6c2db
75tyle <kamblesanghpal2003@gmail.com>
2023-05-05 00:18:17 +0530

    Add project files.

---
+++ connectmydql/Context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using connectmydql.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace connectmydql.Context
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+
+        }
+        public DbSet<Books> books { get; set; }
+    }
+}
---
+++ connectmydql/Model/Books.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace connectmydql.Model
+{
+    public class Books
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
